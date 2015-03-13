505a54b7019122bc47e0ee8e456e5fa3ec8a57bf
brianwernick <brianw@mediarain.com>
2015-03-13 15:05:28 -0600

    Update to the demo project

--- demo/src/main/java/com/devbrackets/android/recyclerextdemo/ReorderListFragment.java
+++ demo/src/main/java/com/devbrackets/android/recyclerextdemo/ReorderListFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,7 +12,6 @@ import com.devbrackets.android.recyclerext.RecyclerExt;
 import com.devbrackets.android.recyclerext.decoration.ReorderDecoration;
 import com.devbrackets.android.recyclerext.layout.LayoutOrientation;
 import com.devbrackets.android.recyclerext.layout.ListLayoutManager;
-import com.devbrackets.android.recyclerextdemo.util.ReorderDetector;
 import com.devbrackets.android.recyclerextdemo.viewholder.SimpleDragItemViewHolder;
 
 import java.util.LinkedList;
@@ -24,9 +22,7 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class ReorderListFragment extends Fragment {
-
     private RecyclerExt recyclerExt;
-    private ReorderDecoration reorderDecoration;
 
     public static ReorderListFragment newInstance() {
         return new ReorderListFragment();
@@ -46,7 +42,7 @@ public class ReorderListFragment extends Fragment {
         recyclerExt.setLayoutManager(new ListLayoutManager(getActivity(), LayoutOrientation.VERTICAL));
         recyclerExt.setAdapter(new ListAdapter(getActivity()));
 
-        reorderDecoration = new ReorderDecoration(recyclerExt);
+        ReorderDecoration reorderDecoration = new ReorderDecoration(recyclerExt);
         reorderDecoration.setDragHandleId(R.id.simple_drag_item_handle);
         recyclerExt.addItemDecoration(reorderDecoration);
         recyclerExt.addOnItemTouchListener(reorderDecoration);
@@ -70,17 +66,12 @@ public class ReorderListFragment extends Fragment {
 
 
 
-    //TODO: as it currently sits, we need to watch the motionEvents to look for an up-event to stop reordering...
-    //      Is there a way around this?
-    private class ListAdapter extends RecyclerView.Adapter<SimpleDragItemViewHolder> implements View.OnTouchListener, ReorderDetector.ReorderDetectorListener {
+    private class ListAdapter extends RecyclerView.Adapter<SimpleDragItemViewHolder> {
         private List<String> examples;
         private LayoutInflater inflater;
-        private ReorderDetector reorderDetector;
 
         public ListAdapter(Context context) {
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            reorderDetector = new ReorderDetector(this);
-
             examples = new LinkedList<>();
 
             for (int i = 1; i < 20; i++) {
@@ -99,48 +90,11 @@ public class ReorderListFragment extends Fragment {
         public void onBindViewHolder(SimpleDragItemViewHolder holder, int position) {
             holder.setText(examples.get(position));
             holder.setPosition(position);
-            //holder.setOnTouchListener(this);
         }
 
         @Override
         public int getItemCount() {
             return examples.size();
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            return reorderDetector.onTouch(v, event);
-        }
-
-        @Override
-        public void onCancel(View view) {
-            reorderDecoration.endReorder();
-        }
-
-        @Override
-        public void onClick(View view) {
-            //Purposefully left blank
-        }
-
-        @Override
-        public void onDone(View view) {
-            reorderStarted = false;
-            int newPosition = reorderDecoration.calculateNewPosition();
-            reorderDecoration.endReorder();
-        }
-
-        @Override
-        public void onLongClick(View view) {
-            if (!reorderStarted) {
-                reorderStarted = true;
-                reorderDecoration.startReorder(view, null);
-            }
-        }
-
-        private boolean reorderStarted;
-        @Override
-        public void onMove(View view, MotionEvent event) {
-
         }
     }
 }
