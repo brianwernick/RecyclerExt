6a0a695c9d8e883c15143386a5d71d4bfc41e9a8
hansenji <jordanihansen10@gmail.com>
2016-11-30 14:55:25 -0700

    Added StatefulItemTouchHelper

---
+++ library/src/main/java/android/support/v7/widget/helper/StatefulItemTouchHelper.java
@@ -0,0 +1,33 @@
+package android.support.v7.widget.helper;
+
+import android.support.v7.widget.RecyclerView;
+
+public class StatefulItemTouchHelper extends ItemTouchHelper {
+
+    private int actionState = ItemTouchHelper.ACTION_STATE_IDLE;
+    private OnActionStateChangedListener onActionStateChangedListener;
+
+
+    public StatefulItemTouchHelper(Callback callback) {
+        super(callback);
+    }
+
+    @Override
+    void select(RecyclerView.ViewHolder selected, int actionState) {
+        if (this.actionState != actionState) {
+            if (onActionStateChangedListener != null) {
+                onActionStateChangedListener.onActionStateChanged(actionState);
+            }
+        }
+        this.actionState = actionState;
+        super.select(selected, actionState);
+    }
+
+    public void setOnActionStateChangedListener(OnActionStateChangedListener onActionStateChangedListener) {
+        this.onActionStateChangedListener = onActionStateChangedListener;
+    }
+
+    public interface OnActionStateChangedListener {
+        void onActionStateChanged(int actionState);
+    }
+}
